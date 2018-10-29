8eaa0fd6b55484a971284f524ca9cacd512be8ad
Sarah Beirkdar <sarahbeirkdar@Sarahs-MacBook-Pro.local>
2018-10-29 12:40:34 +0400

    initial commit

---
+++ RecipeBookApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecipeBookApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
---
+++ RecipeBookApp/Models/IndexModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeBookApp.Models
+{
+    public class IndexModel : PageModel
+    {
+        public string GitHubAvatar { get; set; }
+
+        public string GitHubLogin { get; set; }
+
+        public string GitHubName { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public void OnGet()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                GitHubName = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                GitHubLogin = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+                GitHubUrl = User.FindFirst(c => c.Type == "urn:github:url")?.Value;
+                GitHubAvatar = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+            }
+        }
+    }
+}
